324ca37db881bc1d1e3a369a8f76c04f573b018f
Shirlette Chambers <shirlette.chambers@gmail.com>
2017-12-28 12:32:21 -0500

    functionality for counter to 5 added

--- SimpleClickGame/scoreKeeper.js
+++ SimpleClickGame/scoreKeeper.js
@@ -2,12 +2,36 @@
 
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
-var p1Display = document.querySelector("h1");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
 var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
 
 p1Button.addEventListener("click", function() {
-    // alert("clicked");
-    p1Score++;
-    console.log(p1Score);
-    h1.textContent = p1Score;
+    if (!gameOver) {
+        // alert("clicked");
+        p1Score++;
+        if (p1Score === winningScore) {
+            p1Display.classList.add("winner");
+            // console.log("game over");
+            gameOver = true;
+        }
+        // console.log(p1Score);
+        p1Display.textContent = p1Score;
+    }
+})
+
+p2Button.addEventListener("click", function() {
+    if (!gameOver) {
+        p2Display.classList.add("winner");
+        // alert("p2 clicked");
+        p2Score++;
+        if (p2Score === winningScore) {
+            // console.log(p1Score);
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
 })
