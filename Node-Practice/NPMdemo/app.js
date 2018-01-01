5cf5b01a2cca0cf9fc201d7c1fa6b22e442c7482
Shirlette Chambers <shirlette.chambers@gmail.com>
2018-01-01 16:34:18 -0500

    use faker npm package

--- Node-Practice/NPMdemo/app.js
+++ Node-Practice/NPMdemo/app.js
@@ -1,6 +1,16 @@
 var cat = require("cat-me");
 var joke = require("knock-knock-jokes");
+var faker = require("faker");
 
-console.log(cat());
-console.log(joke());
 // console.log("from app.js");
+// console.log(cat());
+// console.log(joke());
+
+console.log("=======================");
+console.log("WELCOME TO MY SHOP");
+console.log("=======================");
+
+for (var i = 0; i <= 10; i++) {
+    // console.log(randomProduct + " - $" + randomPrice);
+    console.log(faker.commerce.productName() + " - $" + faker.commerce.price());
+}
