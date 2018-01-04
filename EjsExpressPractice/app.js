d0364be9de657dfd4181deee6c88dbce5781b175
Shirlette Chambers <shirlette.chambers@gmail.com>
2018-01-03 22:27:00 -0500

    use ejs to loop through data and get user response

--- EjsExpressPractice/app.js
+++ EjsExpressPractice/app.js
@@ -7,6 +7,15 @@ app.get("/", function(req, res) {
     // res.send("<h1Welcome to the Home Page</h1>");
 });
 
+app.get("/posts", function(req, res) {
+    var posts = [
+        { title: "I like puppies", author: "Shirl" },
+        { title: "What is dis", author: "Tam" },
+        { title: "I'm not here for this", author: "Kish" }
+    ];
+    res.render("posts.ejs", { posts: posts });
+});
+
 app.get("/fallinlovewith/:thing", function(req, res) {
     var thing = req.params.thing;
     // res.send("you fell in love with " + thing);
@@ -18,5 +27,5 @@ app.get("/fallinlovewith/:thing", function(req, res) {
 
 
 app.listen(3000, function() {
-    console.log("running server on prot 3000");
+    console.log("running server on port 3000");
 });
