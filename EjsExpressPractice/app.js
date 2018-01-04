61cd30b2dc1874ec43fe325d7f8a36b074562acc
Shirlette Chambers <shirlette.chambers@gmail.com>
2018-01-03 22:46:40 -0500

    use ejs to add custom assest- header and footer to each view

--- EjsExpressPractice/app.js
+++ EjsExpressPractice/app.js
@@ -1,10 +1,21 @@
 var express = require("express");
 var app = express();
 
+// tell the express app to use the files of the public directory
+app.use(express.static("public"));
+//tell express to use ejs so .ejs is not needed in the routes
+app.set("view engine", "ejs")
 
+// root route
 app.get("/", function(req, res) {
-    res.render("home.ejs");
+    res.render("home.");
     // res.send("<h1Welcome to the Home Page</h1>");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res) {
+    var thing = req.params.thing;
+    // res.send("you fell in love with " + thing);
+    res.render("love", { thingVar: thing });
 });
 
 app.get("/posts", function(req, res) {
@@ -13,16 +24,8 @@ app.get("/posts", function(req, res) {
         { title: "What is dis", author: "Tam" },
         { title: "I'm not here for this", author: "Kish" }
     ];
-    res.render("posts.ejs", { posts: posts });
+    res.render("posts", { posts: posts });
 });
-
-app.get("/fallinlovewith/:thing", function(req, res) {
-    var thing = req.params.thing;
-    // res.send("you fell in love with " + thing);
-    res.render("love.ejs", { thingVar: thing });
-});
-
-
 
 
 
