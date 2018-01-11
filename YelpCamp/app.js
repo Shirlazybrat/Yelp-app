2a68221646c8291d0b6fccac6c895b834e8c936f
Shirlette Chambers <shirlette.chambers@gmail.com>
2018-01-10 21:17:49 -0500

    setup of the views and partials of the application

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 
+var campgrounds = [
+    { name: "Billy goat's Gruff", image: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?h=350&auto=compress&cs=tinysrgb" },
+    { name: "Tablerock Village", image: "https://images.pexels.com/photos/176381/pexels-photo-176381.jpeg?h=350&auto=compress&cs=tinysrgb" },
+    { name: "Mountain View Park", image: "https://images.pexels.com/photos/6714/light-forest-trees-morning.jpg?h=350&auto=compress&cs=tinysrgb" }
+]
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 
@@ -9,12 +17,21 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
-        { name: "Billy goat's Gruff", image: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?h=350&auto=compress&cs=tinysrgb" },
-        { name: "Tablerock Village", image: "https://images.pexels.com/photos/176381/pexels-photo-176381.jpeg?h=350&auto=compress&cs=tinysrgb" },
-        { name: "Mountain View Park", image: "https://images.pexels.com/photos/6714/light-forest-trees-morning.jpg?h=350&auto=compress&cs=tinysrgb" }
-    ]
     res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", function(req, res) {
+    // get data from form to add to arrary
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = { name: name, image: image };
+    campgrounds.push(newCampground);
+    // redirect to back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs")
 });
 
 app.listen(3000, function(req, res) {
